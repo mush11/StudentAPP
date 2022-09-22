47d280b8c7e4b7d7d1f2c164cde02ab614f1cea2
mush11 <mushfikur1123@gmail.com>
2022-09-22 20:33:58 +0600

    setup infrastructure

---
+++ StudentAPP.Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StudentAPP.Infrastructure.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentAPP.Infrastructure.Data
+{
+    internal class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> option) : base(option)
+        {
+
+        }
+
+        public DbSet<Gender> Gender { get; set; }
+        public DbSet<StudentInfo> StudentInfo { get; set; }
+    }
+}
---
+++ StudentAPP.Infrastructure/Models/StudentInfo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentAPP.Infrastructure.Models
+{
+    public class StudentInfo
+    {
+        public int StudentID { get; set; }
+        public string StudentName { get; set; }
+        public string Address { get; set; }
+
+        public int GenderID { get; set; }
+
+        public Gender Gender { get; set; }
+    }
+}
